49e0658f85f80099fc82bfe3637fa66bf15d585d
xiech <xiechanghong@everimaging.com>
2019-01-19 13:50:27 +0800

    增加启动命令控制

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python3
 # encoding: utf-8
+
+import argparse
 import time, threading
 from common.database import RedisClients,ElasticsClient
 from metadata_storage import MetadataStorage
 import app
 
-if __name__ == "__main__":
-  
-    me = MetadataStorage("bt_metadata","doc",10000)
-    me.init_index()
-    t2 = threading.Thread(target=me.start)
-    t2.start()
+def get_parser():
+    parser = argparse.ArgumentParser(description="start main.py with flag.")
+    parser.add_argument(
+        "-w", action="store_true", help="run web_server func."
+    )
+    parser.add_argument(
+        "-m", action="store_true", help="run metadata_storage_task func"
+    )
 
-    app = app.create_app()
-    t1 = threading.Thread(target=app.run,args=('0.0.0.0',8000))
-    t1.start()
+    parser.add_argument(
+        "-a", action="store_true", help="run web_server & metadata_storage_task func"
+    )
+
+    return parser
+
+
+def command_line_runner():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+
+    if args["w"]:
+        app = app.create_app()
+        t1 = threading.Thread(target=app.run,args=('0.0.0.0',8000))
+        t1.start()
+    elif args["m"]:
+        me = MetadataStorage(10)
+        me.init_index()
+        t2 = threading.Thread(target=me.start)
+        t2.start()
+    elif args["a"]:
+        me = MetadataStorage(10)
+        me.init_index()
+        t2 = threading.Thread(target=me.start)
+        t2.start()
+
+        app = app.create_app()
+        t1 = threading.Thread(target=app.run,args=('0.0.0.0',8000))
+        t1.start()
+
+if __name__ == "__main__":
+    command_line_runner()
+    
+
+    
