507ce8707c13fd6ad4f896e945e5affd992af133
xiech <xiechanghong@everimaging.com>
2019-01-20 19:21:19 +0800

    调整定时任务执行频率60秒执行1次

--- main.py
+++ main.py
@@ -37,12 +37,12 @@ def command_line_runner():
         t1 = threading.Thread(target=app.run,args=('0.0.0.0',port))
         t1.start()
     elif args["m"]:
-        me = MetadataStorage(2)
+        me = MetadataStorage(60)
         me.init_index()
         t2 = threading.Thread(target=me.start)
         t2.start()
     elif args["a"]:
-        me = MetadataStorage(2)
+        me = MetadataStorage(60)
         me.init_index()
         t2 = threading.Thread(target=me.start)
         t2.start()
