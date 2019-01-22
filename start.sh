486dd5ae0ffba52091493d011984e69abf8ed1a9
xiech <xiechanghong@everimaging.com>
2019-01-22 15:21:40 +0800

    增加启动脚本

---
+++ start.sh
@@ -0,0 +1,11 @@
+LANG="zh_CN.UTF-8"
+pid=`ps ax | grep main | grep python | grep -v grep | head -1 | awk '{print $1}'`
+if [ "$pid" != "" ]; then
+	echo $pid
+	kill $pid
+fi
+echo '服务停止成功,开始重启服务...'
+rm -rf nohup.out
+touch nohup.out
+nohup python3 main.py -w -port=80 > nohup.out 2>&1 &
+tail -f nohup.out
