71d8c7c54f6b6a151c55eacfb078c960f0281681
xiech <xiechanghong@everimaging.com>
2019-01-22 15:33:42 +0800

    增加启动配置

--- start.sh
+++ start.sh
@@ -8,12 +8,14 @@ echo '服务停止成功,开始重启服务...'
 rm -rf nohup.out
 touch nohup.out
 
-echo $1
 if [ ! "$1" ]; then  
     $1='-a'
 fi
 if [ ! "$2" ]; then  
     $2='80'
 fi
+
+echo $1
+echo $2
 nohup python3 main.py $1 -port=$2 > nohup.out 2>&1 &
 tail -f nohup.out
